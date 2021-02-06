39cb82e7303118e7727bea0614a2ce3b8caf8078
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-06 16:17:23 -0500

    commit tree 846b9331ee0e11d57cc8e1db451f111ee12209e6

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
+require 'pry'
+
 # Write your code here.
+def line(array) 
+    if array.size > 0
+        new_array = array.collect.with_index(1) do |person, index|
+            "#{index}. #{person}"
+        end
+
+
+        puts "The line is currently: #{new_array.join(" ")}"
+    else
+        puts "The line is currently empty."
+    end
+end
+
+def take_a_number(array, name)
+    array << name
+            puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+    if array.length > 0 
+        puts "Currently serving #{array.shift}."
+    else
+        puts "There is nobody waiting to be served!"
+    end
+end
